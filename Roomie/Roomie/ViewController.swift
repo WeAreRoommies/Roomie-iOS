d99f182e14273a70d1e9c495979bf1f617f8db05
mmaybei <yebbi2@ewhain.net>
2025-01-07 13:12:52 +0900

    [setting/#3] 스토리보드 연결 해제

---
+++ Roomie/Roomie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Roomie
+//
+//  Created by 예삐 on 1/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
