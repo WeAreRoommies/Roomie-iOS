cb71e1f71a982bc8a4a66ec5b66bc50f44a1f09d
won <andrew112517@naver.com>
2025-01-08 16:16:37 +0900

    [hotfix] .gitkeep오류 수정

---
+++ Roomie/Roomie/Global/Components/TempComponents.swift
@@ -0,0 +1,8 @@
+//
+//  TempComponents.swift
+//  Roomie
+//
+//  Created by 김승원 on 1/8/25.
+//
+
+import Foundation
---
+++ Roomie/Roomie/Global/Enums/TempEnums.swift
@@ -0,0 +1,8 @@
+//
+//  TempEnums.swift
+//  Roomie
+//
+//  Created by 김승원 on 1/8/25.
+//
+
+import Foundation
