905038245acc5b49d89a2b6e029ba33de274e30c
won <andrew112517@naver.com>
2025-01-15 13:16:50 +0900

    [feat/#30] TourViewModel 구현

--- Roomie/Roomie/Presentation/Tour/ViewModel/TourViewModel.swift
+++ Roomie/Roomie/Presentation/Tour/ViewModel/TourViewModel.swift
@@ -13,6 +13,7 @@ final class TourViewModel {
     // MARK: - Property
     
     private let nameTextSubject = PassthroughSubject<String, Never>()
+    private let dateSubject = PassthroughSubject<String, Never>()
     private let genderSubject = PassthroughSubject<Gender, Never>()
     private let phoneNumberTextSubject = PassthroughSubject<String, Never>()
     
@@ -20,25 +21,27 @@ final class TourViewModel {
 
 extension TourViewModel: ViewModelType {
     struct Input {
-        // TODO: DatePicker값 받기
         let nameTextSubject: AnyPublisher<String, Never>
-        let phoneNumberTextSubject: AnyPublisher<String, Never>
+        let dateSubject: AnyPublisher<String, Never>
         let genderSubject: AnyPublisher<Gender, Never>
-        
-        // TODO: 투어신청 3 input 설정
+        let phoneNumberTextSubject: AnyPublisher<String, Never>
     }
     
     struct Output {
         let isPhoneNumberValid: AnyPublisher<Bool, Never>
         let isNextButtonEnabled: AnyPublisher<Bool, Never>
-        
-        // TODO: 투어신청 3 Output 설정
     }
     
     func transform(from input: Input, cancelBag: CancelBag) -> Output {
         input.nameTextSubject
             .sink { [weak self] name in
                 self?.nameTextSubject.send(name)
+            }
+            .store(in: cancelBag)
+        
+        input.dateSubject
+            .sink { [weak self] date in
+                self?.dateSubject.send(date)
             }
             .store(in: cancelBag)
         
@@ -64,13 +67,14 @@ extension TourViewModel: ViewModelType {
             }
             .eraseToAnyPublisher()
         
-        let isNextButtonEnabled = Publishers.CombineLatest3(
+        let isNextButtonEnabled = Publishers.CombineLatest4(
             self.nameTextSubject.map { !$0.isEmpty },
+            self.dateSubject.map { !$0.isEmpty },
             self.genderSubject.map { $0 != .none },
             isPhoneNumberValid
         )
-            .map { isNameValid, isgenderValid, isPhoneNumberValid in
-                isNameValid && isgenderValid && isPhoneNumberValid
+            .map { isNameValid, isDateValid, isGenderValid, isPhoneNumberValid in
+                isNameValid && isDateValid && isGenderValid && isPhoneNumberValid
             }
             .eraseToAnyPublisher()
         
