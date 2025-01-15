7e223968868d707a0a19a0232a167954be6b7799
won <andrew112517@naver.com>
2025-01-15 16:11:23 +0900

    [feat/#37] TourDateView UI TextView제외 UI 구현

--- Roomie/Roomie/Presentation/Tour/View/TourDateView.swift
+++ Roomie/Roomie/Presentation/Tour/View/TourDateView.swift
@@ -12,16 +12,60 @@ import Then
 
 final class TourDateView: BaseView {
     
+    // MARK: - UIComponent
+    
+    private let titleLabel = UILabel()
+    
+    private let hopeDateLabel = UILabel()
+    private let hopeDatePickerView = DatePickerView()
+    
+    private let messageLabel = UILabel()
+    
     // MARK: - UISetting
     
     override func setStyle() {
+        titleLabel.do {
+            $0.setText("입주희망일과\n문의하고 싶은 내용을 적어주세요", style: .heading2, color: .grayscale12)
+        }
+        
+        hopeDateLabel.do {
+            $0.setText("입주 희망날짜", style: .body2, color: .grayscale10)
+        }
+        
+        messageLabel.do {
+            $0.setText("문의내용", style: .body2, color: .grayscale10)
+        }
     }
     
     override func setUI() {
-        
+        addSubviews(
+            titleLabel,
+            hopeDateLabel,
+            hopeDatePickerView,
+            messageLabel
+        )
     }
     
     override func setLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide.snp.top).inset(20)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
         
+        hopeDateLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(54)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
+        
+        hopeDatePickerView.snp.makeConstraints {
+            $0.top.equalTo(hopeDateLabel.snp.bottom).offset(12)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(Screen.height(54))
+        }
+        
+        messageLabel.snp.makeConstraints {
+            $0.top.equalTo(hopeDatePickerView.snp.bottom).offset(32)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
     }
 }
