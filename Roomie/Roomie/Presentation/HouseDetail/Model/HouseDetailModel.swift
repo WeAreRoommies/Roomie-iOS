0cdb943e9d95e7f823bd07677debc8e66f829e7c
won <andrew112517@naver.com>
2025-01-17 01:04:30 +0900

    [chore/#46] Model UUID 삭제, Hashable 채택 취소
    diffableDataSource 사용할 이유가 없기 때문에 수정합니다.

--- Roomie/Roomie/Presentation/HouseDetail/Model/HouseDetailModel.swift
+++ Roomie/Roomie/Presentation/HouseDetail/Model/HouseDetailModel.swift
@@ -9,8 +9,7 @@ import Foundation
 
 // MARK: - HouseDetailModel
 
-struct HouseDetailModel: Hashable {
-    let id = UUID()
+struct HouseDetailModel {
     let houseInfo: HouseInfo
     let rooms: [Room]
     let roommates: [Roommate]
@@ -18,8 +17,7 @@ struct HouseDetailModel: Hashable {
 
 // MARK: - House Info
 
-struct HouseInfo: Hashable {
-    let id = UUID()
+struct HouseInfo {
     let houseId: Int
     let name: String
     let mainImgUrl: String
@@ -41,8 +39,7 @@ struct HouseInfo: Hashable {
 
 // MARK: - Room
 
-struct Room: Hashable {
-    let id = UUID()
+struct Room {
     let roomId: Int
     let name: String
     let status: Bool
@@ -57,8 +54,7 @@ struct Room: Hashable {
 
 // MARK: - Roommate
 
-struct Roommate: Hashable {
-    let id = UUID()
+struct Roommate {
     let name: String
     let age: Int
     let job: String
@@ -70,73 +66,71 @@ struct Roommate: Hashable {
 // MARK: - Mock Data
 
 extension HouseDetailModel {
-    static func mockData() -> [HouseDetailModel] {
-        return [
-            HouseDetailModel(
-                houseInfo: HouseInfo(
-                    houseId: 123,
-                    name: "루미 100호점(이대역)",
-                    mainImgUrl: "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDA1MTJfNDMg%2FMDAxNzE1NTA2ODczMjAy.sMMENVnkUx2jpoJtEZtddKoIRSVyYEjRxGA81vWPlPMg.HCWsC4FSH2up1BJJGDeDzwzPFTxa7M3Qv1bBb0YS7eAg.JPEG%2FPDM_0612.jpg&type=sc960_832",
-                    monthlyRent: "43~50",
-                    deposit: "90~100",
-                    location: "서대문구 연희동",
-                    occupancyTypes: "1,2,3인실",
-                    occupancyStatus: "2/5",
-                    genderPolicy: "여성 전용",
-                    contractTerm: 3,
-                    moodTags: ["#차분한", "#유쾌한", "#경쾌한"],
-                    roomMood: "전반적으로 조용하고 깔끔한 환경을 선호하는 아침형 룸메이트들이에요.",
-                    groundRule: ["요리 후 바로 설거지해요", "청소는 주3회 돌아가면서 해요"],
-                    maintenanceCost: 300000,
-                    isPinned: true,
-                    safetyLivingFacility: ["소화기", "소화기"],
-                    kitchenFacility: ["주걱", "밥솥"]
+    static func mockData() -> HouseDetailModel {
+        return HouseDetailModel(
+            houseInfo: HouseInfo(
+                houseId: 123,
+                name: "루미 100호점(이대역)",
+                mainImgUrl: "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDA1MTJfNDMg%2FMDAxNzE1NTA2ODczMjAy.sMMENVnkUx2jpoJtEZtddKoIRSVyYEjRxGA81vWPlPMg.HCWsC4FSH2up1BJJGDeDzwzPFTxa7M3Qv1bBb0YS7eAg.JPEG%2FPDM_0612.jpg&type=sc960_832",
+                monthlyRent: "43~50",
+                deposit: "90~100",
+                location: "서대문구 연희동",
+                occupancyTypes: "1,2,3인실",
+                occupancyStatus: "2/5",
+                genderPolicy: "여성 전용",
+                contractTerm: 3,
+                moodTags: ["#차분한", "#유쾌한", "#경쾌한"],
+                roomMood: "전반적으로 조용하고 깔끔한 환경을 선호하는 아침형 룸메이트들이에요.",
+                groundRule: ["요리 후 바로 설거지해요", "청소는 주3회 돌아가면서 해요"],
+                maintenanceCost: 300000,
+                isPinned: true,
+                safetyLivingFacility: ["소화기", "소화기"],
+                kitchenFacility: ["주걱", "밥솥"]
+            ),
+            rooms: [
+                Room(
+                    roomId: 1,
+                    name: "1A 싱글침대",
+                    status: true,
+                    occupancyType: 2,
+                    gender: "여성",
+                    deposit: 5000000,
+                    prepaidUtilities: 100000,
+                    monthlyRent: 500000,
+                    contractPeriod: "24-12-20",
+                    managementFee: "1/n"
                 ),
-                rooms: [
-                    Room(
-                        roomId: 1,
-                        name: "1A 싱글침대",
-                        status: true,
-                        occupancyType: 2,
-                        gender: "여성",
-                        deposit: 5000000,
-                        prepaidUtilities: 100000,
-                        monthlyRent: 500000,
-                        contractPeriod: "24-12-20",
-                        managementFee: "1/n"
-                    ),
-                    Room(
-                        roomId: 2,
-                        name: "1B 싱글침대",
-                        status: false,
-                        occupancyType: 1,
-                        gender: "여성",
-                        deposit: 5000000,
-                        prepaidUtilities: 100000,
-                        monthlyRent: 500000,
-                        contractPeriod: "24-12-20",
-                        managementFee: "1/n"
-                    )
-                ],
-                roommates: [
-                    Roommate(
-                        name: "1A 싱글침대",
-                        age: 23,
-                        job: "대학생",
-                        mbti: "ENFP",
-                        sleepTime: "21:00-21:00",
-                        activityTime: "21:00-21:00"
-                    ),
-                    Roommate(
-                        name: "1A 싱글침대",
-                        age: 23,
-                        job: "대학생",
-                        mbti: "ENFP",
-                        sleepTime: "21:00-21:00",
-                        activityTime: "21:00-21:00"
-                    )
-                ]
-            )
-        ]
+                Room(
+                    roomId: 2,
+                    name: "1B 싱글침대",
+                    status: false,
+                    occupancyType: 1,
+                    gender: "여성",
+                    deposit: 5000000,
+                    prepaidUtilities: 100000,
+                    monthlyRent: 500000,
+                    contractPeriod: "24-12-20",
+                    managementFee: "1/n"
+                )
+            ],
+            roommates: [
+                Roommate(
+                    name: "1A 싱글침대",
+                    age: 23,
+                    job: "대학생",
+                    mbti: "ENFP",
+                    sleepTime: "21:00-21:00",
+                    activityTime: "21:00-21:00"
+                ),
+                Roommate(
+                    name: "1A 싱글침대",
+                    age: 23,
+                    job: "대학생",
+                    mbti: "ENFP",
+                    sleepTime: "21:00-21:00",
+                    activityTime: "21:00-21:00"
+                )
+            ]
+        )
     }
 }
