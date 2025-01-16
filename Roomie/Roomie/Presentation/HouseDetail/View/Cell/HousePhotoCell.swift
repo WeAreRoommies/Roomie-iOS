a1a2bc281bd9aef53365ab16b5502e29dd0d2030
won <andrew112517@naver.com>
2025-01-16 21:21:02 +0900

    [feat/#46] HousePhotoCell 구현

--- Roomie/Roomie/Presentation/HouseDetail/View/Cell/HousePhotoCell.swift
+++ Roomie/Roomie/Presentation/HouseDetail/View/Cell/HousePhotoCell.swift
@@ -12,17 +12,43 @@ import Then
 
 class HousePhotoCell: BaseCollectionViewCell {
     
+    // MARK: - UIComponent
+    
+    private let photoImageView = UIImageView()
+    
+    private let roundedTopView = UIView()
+    
+    // MARK: - UISetting
+    
     override func setStyle() {
         self.do {
             $0.backgroundColor = .red
         }
+        
+        photoImageView.do {
+            $0.backgroundColor = .grayscale4
+        }
+        
+        roundedTopView.do {
+            $0.backgroundColor = .grayscale1
+            $0.roundCorners(cornerRadius: 8, maskedCorners: [.layerMinXMinYCorner, .layerMaxXMinYCorner])
+        }
     }
     
     override func setUI() {
-        
+        addSubview(photoImageView)
+        photoImageView.addSubview(roundedTopView)
     }
     
     override func setLayout() {
+        photoImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
         
+        roundedTopView.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(Screen.height(20))
+        }
     }
 }
