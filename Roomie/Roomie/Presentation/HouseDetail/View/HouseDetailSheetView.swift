af63192a7e1313401bb9659f561b002548b58f9a
won <andrew112517@naver.com>
2025-01-21 21:06:11 +0900

    [feat/#96] HouseDetailSheetView 추가

---
+++ Roomie/Roomie/Presentation/HouseDetail/View/HouseDetailSheetView.swift
@@ -0,0 +1,31 @@
+//
+//  HouseDetailSheetView.swift
+//  Roomie
+//
+//  Created by 김승원 on 1/21/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class HouseDetailSheetView: BaseView {
+    
+    // MARK: - UIComponent
+    
+    // MARK: - UISetting
+    
+    override func setStyle() {
+        
+    }
+    
+    override func setUI() {
+        
+    }
+    
+    override func setLayout() {
+        
+    }
+    
+}
