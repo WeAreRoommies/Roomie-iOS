23a472f3348df24c239e18c15c27c8e8d0069da1
won <andrew112517@naver.com>
2025-01-22 02:02:41 +0900

    [feat/#96] HouseDetailViewModel 구현

--- Roomie/Roomie/Presentation/HouseDetail/ViewModel/HouseDetailViewModel.swift
+++ Roomie/Roomie/Presentation/HouseDetail/ViewModel/HouseDetailViewModel.swift
@@ -9,5 +9,33 @@ import Foundation
 import Combine
 
 final class HouseDetailViewModel {
+    // MARK: - Property
     
+    private let roomIDSubject = PassthroughSubject<Int, Never>()
+    
+}
+
+extension HouseDetailViewModel: ViewModelType {
+    struct Input {
+        let roomIDSubject: AnyPublisher<Int, Never>
+    }
+    
+    struct Output {
+        let isTourApplyButtonEnabled: AnyPublisher<Bool, Never>
+    }
+    
+    func transform(from input: Input, cancelBag: CancelBag) -> Output {
+        input.roomIDSubject
+            .sink { [weak self] roomID in
+                guard let self else { return }
+                self.roomIDSubject.send(roomID)
+            }
+            .store(in: cancelBag)
+        
+        let isTourApplyButtonEnabled = self.roomIDSubject
+            .map { _ in true }
+            .eraseToAnyPublisher()
+        
+        return Output(isTourApplyButtonEnabled: isTourApplyButtonEnabled)
+    }
 }
