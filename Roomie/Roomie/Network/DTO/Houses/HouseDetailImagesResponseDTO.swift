fca79d586360955a1657bcf3b44284b946b6db04
won <andrew112517@naver.com>
2025-01-23 07:53:14 +0900

    [feat/#121] HouseDetailImagesResponseDTO 구현

---
+++ Roomie/Roomie/Network/DTO/Houses/HouseDetailImagesResponseDTO.swift
@@ -0,0 +1,26 @@
+//
+//  HouseDetailImagesResponseDTO.swift
+//  Roomie
+//
+//  Created by 김승원 on 1/23/25.
+//
+
+import Foundation
+
+struct HouseDetailImagesResponseDTO: ResponseModelType {
+    let images: HouseDetailImages
+}
+
+struct HouseDetailImages: Codable {
+    let mainImageURL: String
+    let mainImageDescription: String
+    let facilityImageURLs: [String]
+    let facilityImageDescription: String
+    let floorImageURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case mainImageURL = "mainImgUrl"
+        case mainImageDescription, facilityImageURLs, facilityImageDescription
+        case floorImageURL = "floorImgUrl"
+    }
+}
