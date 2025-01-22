bfea1f81822daa2234bd0ac0e0f21374870e559e
maeng-kim <ehseo0410@gmail.com>
2025-01-22 19:29:39 +0900

    [feat/#104] MoodList Request, Response DTO 수정

--- Roomie/Roomie/Network/DTO/Houses/MoodListResponseDTO.swift
+++ Roomie/Roomie/Network/DTO/Houses/MoodListResponseDTO.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 struct MoodListResponseDTO: ResponseModelType {
-    
+    let moodTag: String
+    let houses: [MoodHouse]
+}
+
+struct MoodHouse: Codable, Hashable {
+    let houseID: Int
+    let monthlyRent, deposit, occupancyTypes, location: String
+    let genderPolicy, locationDescription: String
+    let isPinned: Bool
+    let contractTerm: Int
+    let mainImgURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case houseID = "houseId"
+        case monthlyRent, deposit, occupancyTypes, location, genderPolicy, locationDescription, isPinned
+        case contractTerm = "contract_term"
+        case mainImgURL = "mainImgUrl"
+    }
 }
