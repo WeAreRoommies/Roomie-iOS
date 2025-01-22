67fd98e90dd6a6ce376f7ffd155237179603e831
won <andrew112517@naver.com>
2025-01-23 04:27:35 +0900

    [feat/#112] RoomMoodInfo 모델 구현

---
+++ Roomie/Roomie/Presentation/HouseDetail/Model/RoomMoodInfo.swift
@@ -0,0 +1,14 @@
+//
+//  RoomMoodInfo.swift
+//  Roomie
+//
+//  Created by 김승원 on 1/23/25.
+//
+
+import Foundation
+
+struct RoomMoodInfo {
+    let roomMood: String
+    let moodTags: [String]
+    let groundRule: [String]
+}
