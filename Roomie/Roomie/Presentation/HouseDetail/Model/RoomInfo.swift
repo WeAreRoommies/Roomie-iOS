cf471fc59b928313df0d7a93ebc9aa581b107411
won <andrew112517@naver.com>
2025-01-23 04:26:33 +0900

    [feat/#112] RoomInfo 모델 구현

---
+++ Roomie/Roomie/Presentation/HouseDetail/Model/RoomInfo.swift
@@ -0,0 +1,20 @@
+//
+//  RoomInfo.swift
+//  Roomie
+//
+//  Created by 김승원 on 1/23/25.
+//
+
+import Foundation
+
+struct RoomInfo {
+    let roomID: Int
+    let status: Bool
+    let name: String
+    let roomType: String
+    let deposit: String
+    let prepaidUtilities: String
+    let monthlyRent: String
+    let contractPeriod: String
+    let managementFee: String
+}
