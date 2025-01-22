9da28dd770037d06f55451fbcf32c3a61a57a12b
won <andrew112517@naver.com>
2025-01-23 08:55:36 +0900

    [feat/#121] HouseDetailServiceProtocol 함수 추가

--- Roomie/Roomie/Presentation/HouseDetail/ServiceProtocol/HouseDetailServiceProtocol.swift
+++ Roomie/Roomie/Presentation/HouseDetail/ServiceProtocol/HouseDetailServiceProtocol.swift
@@ -10,5 +10,7 @@ import Foundation
 protocol HouseDetailServiceProtocol {
     func fetchHouseDetailData(houseID: Int) async throws -> BaseResponseBody<HouseDetailResponseDTO>?
     
+    func fetchHouseDetailImagesData(houseID: Int) async throws -> BaseResponseBody<HouseDetailImagesResponseDTO>?
+    
     // TODO: 찜 API 추가
 }
