967b97eb2e5a09de100f4318ad5d4d5e58df3761
won <andrew112517@naver.com>
2025-01-22 19:00:05 +0900

    [feat/#106] HouseDetail TargetType 추가

--- Roomie/Roomie/Network/TargetType/HousesTargetType.swift
+++ Roomie/Roomie/Network/TargetType/HousesTargetType.swift
@@ -11,17 +11,20 @@ import Moya
 
 enum HousesTargetType {
     case fetchWishLishData
+    case fetchHouseDetailData(houseID: Int)
 }
 
 extension HousesTargetType: TargetType {
     var baseURL: URL {
-        return URL(string: Environment.baseURL)!
+        return URL(string: "\(Environment.baseURL)/v1/houses")!
     }
     
     var path: String {
         switch self {
         case .fetchWishLishData:
-            return "/v1/houses/pins"
+            return "/pins"
+        case .fetchHouseDetailData(houseID: let houseID):
+            return "/\(houseID)/details"
         }
     }
     
@@ -29,12 +32,16 @@ extension HousesTargetType: TargetType {
         switch self {
         case .fetchWishLishData:
             return .get
+        case .fetchHouseDetailData(houseID: let houseID):
+            return .get
         }
     }
     
     var task: Moya.Task {
         switch self {
         case .fetchWishLishData:
+            return .requestPlain
+        case .fetchHouseDetailData:
             return .requestPlain
         }
     }
