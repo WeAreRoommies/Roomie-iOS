6342bdbc3536984fd3c8e1b950a0a0919a6761aa
won <andrew112517@naver.com>
2025-01-22 13:01:25 +0900

    [feat/#98] HouseAllPhotoView 구현

---
+++ Roomie/Roomie/Presentation/HouseDetail/ViewController/HouseAllPhotoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HouseAllPhotoViewController.swift
+//  Roomie
+//
+//  Created by 김승원 on 1/22/25.
+//
+
+import UIKit
+
+final class HouseAllPhotoViewController: BaseViewController {
+    
+    // MARK: - Property
+    
+    private let rootView = HouseAllPhotoView()
+    
+    // MARK: - LifeCycle
+    
+    override func loadView() {
+        view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        rootView.fetchRooms(RoomDetail.mockData())
+    }
+    
+    override func setView() {
+        setNavigationBar(with: "43~50/90~100", isBorderHidden: false)
+    }
+}
