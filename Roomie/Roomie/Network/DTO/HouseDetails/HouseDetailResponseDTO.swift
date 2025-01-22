42ab6b8ee661434fcc13daa3f4c359a464552282
won <andrew112517@naver.com>
2025-01-22 19:33:55 +0900

    [feat/#106] HouseDetailResponseDTO 추가

---
+++ Roomie/Roomie/Network/DTO/HouseDetails/HouseDetailResponseDTO.swift
@@ -0,0 +1,63 @@
+//
+//  HouseDetailResponseDTO.swift
+//  Roomie
+//
+//  Created by 김승원 on 1/22/25.
+//
+
+import Foundation
+
+struct HouseDetailResponseDTO: ResponseModelType {
+    let code: Int
+    let message: String
+    let data: DataClass
+}
+
+struct DataClass: Codable {
+    let houseInfo: HouseInfo
+    let rooms: [Room]
+    let roommates: [Roommate]
+}
+
+struct HouseInfo: Codable {
+    let houseID: Int
+    let name, mainImgURL, monthlyRent, deposit: String
+    let location, occupancyTypes, occupancyStatus, genderPolicy: String
+    let contractTerm: Int
+    let moodTags: [String]
+    let roomMood: String
+    let groundRule: [String]
+    let maintenanceCost: Int
+    let isPinned: Bool
+    let safetyLivingFacility, kitchenFacility: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case houseID = "houseId"
+        case name
+        case mainImgURL = "mainImgUrl"
+        case monthlyRent, deposit, location, occupancyTypes, occupancyStatus, genderPolicy, contractTerm
+        case moodTags, roomMood, groundRule, maintenanceCost, isPinned, safetyLivingFacility, kitchenFacility
+    }
+}
+
+struct Roommate: Codable {
+    let name, age, job, mbti: String
+    let sleepTime, activityTime: String
+}
+
+struct Room: Codable {
+    let roomID: Int
+    let name: String
+    let status, isTourAvailable: Bool
+    let occupancyType: Int
+    let gender: String
+    let deposit, prepaidUtilities, monthlyRent: Int
+    let contractPeriod: String?
+    let managementFee: String
+
+    enum CodingKeys: String, CodingKey {
+        case roomID = "roomId"
+        case name, status, isTourAvailable, occupancyType, gender, deposit, prepaidUtilities
+        case monthlyRent, contractPeriod, managementFee
+    }
+}
