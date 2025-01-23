1a981a2cf9206102a09024c245e4d259c4c72c71
won <andrew112517@naver.com>
2025-01-23 21:35:28 +0900

    [feat/#127] RoomButtonInfo 모델 생성

---
+++ Roomie/Roomie/Presentation/HouseDetail/Model/RoomButtonInfo.swift
@@ -0,0 +1,15 @@
+//
+//  RoomButtonModel.swift
+//  Roomie
+//
+//  Created by 김승원 on 1/23/25.
+//
+
+import Foundation
+
+struct RoomButtonInfo {
+    let name: String
+    let status: Bool
+    let isTourAvailable: Bool
+    let subTitle: String
+}
