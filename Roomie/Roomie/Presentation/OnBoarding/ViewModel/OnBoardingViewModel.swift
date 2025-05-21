bcc41d48d159b6a86ed4fbfd9b8c275ec9a3245b
maeng-kim <ehseo0410@gmail.com>
2025-05-21 23:04:49 +0900

    [feat/#158] OnBoardingViewModel 작업

---
+++ Roomie/Roomie/Presentation/OnBoarding/ViewModel/OnBoardingViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  OnBoardingViewModel.swift
+//  Roomie
+//
+//  Created by MaengKim on 5/20/25.
+//
+
+import Foundation
+import Combine
+
+final class OnBoardingViewModel {
+    
+    // MARK: - Property
+    
+    let currentPageSubject = CurrentValueSubject<OnBoardingType, Never>(.page1)
+}
+
+extension OnBoardingViewModel: ViewModelType {
+    struct Input {
+        let currentPageSubject: AnyPublisher<OnBoardingType, Never>
+    }
+    
+    struct Output {
+        let currentPage: AnyPublisher<OnBoardingType, Never>
+    }
+    
+    func transform(from input: Input, cancelBag: CancelBag) -> Output {
+        input.currentPageSubject
+            .sink { [weak self] type in
+                self?.currentPageSubject.send(type)
+            }
+            .store(in: cancelBag)
+        
+        return Output(
+            currentPage: currentPageSubject.eraseToAnyPublisher()
+        )
+    }
+}
