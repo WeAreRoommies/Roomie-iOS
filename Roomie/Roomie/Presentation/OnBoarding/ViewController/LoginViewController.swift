170f05ec5de9e87d7e10d7cdc0bfba64a7f66dcd
Won <andrew112517@naver.com>
2025-05-27 02:29:08 +0900

    [feat/#166] 애플 로그인 구현

--- Roomie/Roomie/Presentation/OnBoarding/ViewController/LoginViewController.swift
+++ Roomie/Roomie/Presentation/OnBoarding/ViewController/LoginViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import SnapKit
 import Combine
 
+import AuthenticationServices
+
 final class LoginViewController: BaseViewController {
     
     // MARK: - Property
@@ -32,5 +34,70 @@ final class LoginViewController: BaseViewController {
                 self.navigationController?.pushViewController(MainTabBarController(), animated: true)
             }
             .store(in: cancelBag)
+        
+        rootView.appleLoginButton
+            .tapPublisher
+            .sink { [weak self] in
+                guard let self else { return }
+                self.appleLoginButtonDidTap()
+            }
+            .store(in: cancelBag)
+    }
+}
+
+extension LoginViewController {
+    func appleLoginButtonDidTap() {
+        print("애플 로그인 시작")
+        
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = []
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+}
+
+extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+}
+
+extension LoginViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(
+        controller: ASAuthorizationController,
+        didCompleteWithAuthorization authorization: ASAuthorization
+    ) {
+        guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential else {
+            return
+        }
+        
+        let identityToken = appleIDCredential.identityToken.flatMap {
+            String(data: $0, encoding: .utf8)
+        }
+        
+        print("✅ Apple 로그인 성공!!!")
+        print("IdentityToken: \(String(describing: identityToken))")
+        
+        /*
+         Todo: Keychain에 애플 로그인 정보 저장
+         */
+
+        /*
+         Todo: 서버 통신 메서드 호출
+         */
+    }
+    
+    func authorizationController(
+        controller: ASAuthorizationController,
+        didCompleteWithError error: Error
+    ) {
+        /*
+         Todo: 에러 핸들링 (ex. alert창)
+         */
+        print("🚨Apple 로그인 실패: \(error.localizedDescription)")
     }
 }
