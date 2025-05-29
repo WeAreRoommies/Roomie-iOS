290a71a0aba836fb801745745887da864f6ec9ed
Won <andrew112517@naver.com>
2025-05-29 12:37:03 +0900

    [feat/#172] AuthReissueResponseDTO 구현

---
+++ Roomie/Roomie/Network/DTO/Auth/AuthReissueResponseDTO.swift
@@ -0,0 +1,13 @@
+//
+//  AuthReissueResponseDTO.swift
+//  Roomie
+//
+//  Created by 김승원 on 5/29/25.
+//
+
+import Foundation
+
+struct AuthReissueResponseDTO: ResponseModelType {
+    let accessToken: String
+    let refreshToken: String
+}
