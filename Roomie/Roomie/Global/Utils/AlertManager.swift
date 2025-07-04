98d4298cd1d7269f43a0880663e411eb1fe9f7a9
Won <andrew112517@naver.com>
2025-07-04 16:26:32 +0900

    [chore/#185] 주석 수정

--- Roomie/Roomie/Global/Utils/AlertManager.swift
+++ Roomie/Roomie/Global/Utils/AlertManager.swift
@@ -15,9 +15,7 @@ final class AlertManager {
     /// 화면에 알림을 표시합니다
     /// - Parameters:
     ///   - viewController: 알림 표시 대상 UIViewcontroller 인스턴스
-    ///   - title: 알림 제목
-    ///   - message: 알림 내용입니다 기본값은 nil로 빈 문자열
-    ///   - isCancelButtonEnabled: 취소 버튼 표시 여부
+    ///   - alertType: Alert타입
     ///   - confirmHandler: 확인 버튼 후 실행할 클로저
     func showAlert(
         on viewController: UIViewController,
